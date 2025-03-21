e1144811cbb7df3f7aa392847aadf2483eebae1c
Mario Roberto Molano <mariormolano@yahoo.com>
2025-03-21 09:37:36 -0500

    fix: registro con render3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import useAuthToken from "@/features/auth/useAuthToken";
 
 export default function App() {
   const router = useRouter();
 
-  const { isAuthenticated } = useAuthToken();
-
   useEffect(() => {
-    if (!isAuthenticated()) {
-      console.log("No autenticado redirigiendo a login desde home");
-      router.push("/login");
-    } else {
-      console.log("Autenticado redirigiendo a dashboard desde home");
-      router.push("/dashboard");
-    }
+    console.log("Autenticado redirigiendo a dashboard desde home");
+    router.push("/dashboard");
   }, []);
 }
